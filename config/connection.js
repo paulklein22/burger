a719f6749626a840ea9bcad054b2b16d2169e047
PaulKlein22 <paulklein22@gmail.com>
2018-12-18 17:59:56 -0600

    added connection variable

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,7 @@
+require('dotenv').config();
 const mysql = require("mysql");
+var connection;
+
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 const express = require ("express");
-const path = require('path');
+// const path = require('path');
 const exphbs = require("express-handlebars");
-const routes = require("./controllers/burgers_controllers.js");
-const bodyParser = require ("body-parser");
+
 
 const app = express();
-
-app.set("views", path.join(__dirname, "views"));
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
-app.set("view engine", "handlebars");
 app.set("port", (process.env.PORT || 3000));
-app.use(routes);
-app.use(express.static("public"));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 // parse application/json
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.static("public"));
+
+// app.set("views", path.join(__dirname, "views"));
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+const routes = require("./controllers/burgers_controllers.js");
+app.use(routes);
+
 
 app.listen(app.get("port"), function(){
     console.log("Server started on port " +app.get("port"));
